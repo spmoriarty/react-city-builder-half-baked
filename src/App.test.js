4d4ea87c87a7c7203f999c51bdc9350e312f3071
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-13 17:48:27 -0700

    this works

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/castleId/i);
+  const linkElement = screen.getByText(/Welcome to/i);
   expect(linkElement).toBeInTheDocument();
 });
